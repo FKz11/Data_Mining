e1671c74af3e47f02e0870bb30790bff9aa5e855
FKz11 <rjcnz735@yandex.ru>
2021-05-10 02:03:30 +0300

    lesson-5

--- gb_parse/loaders.py
+++ gb_parse/loaders.py
@@ -2,16 +2,26 @@ from json import loads as json_loads
 from html2text import html2text
 
 from scrapy.loader import ItemLoader
-from itemloaders.processors import TakeFirst, MapCompose, Join
+from itemloaders.processors import TakeFirst, MapCompose, Compose, Join
 
 
-def get_description(script):
-    description = html2text(json_loads(script)["description"])
+def get_description(data):
+    description = html2text(json_loads(data)["description"])
     return description
 
 
-def get_activity(string):
-    activity = "".join(string).split(sep=', ')
+def get_author(data):
+    author = "https://hh.ru" + data
+    return author
+
+
+def get_title(data):
+    title = "".join(itm for itm in data if itm != " ")
+    return title
+
+
+def get_activity(data):
+    activity = "".join(data).split(sep=', ')
     return activity
 
 
@@ -24,13 +34,14 @@ class VacancyLoader(ItemLoader):
     price_out = TakeFirst()
     description_in = MapCompose(get_description)
     description_out = TakeFirst()
+    author_in = MapCompose(get_author)
     author_out = TakeFirst()
 
 
 class AuthorLoader(ItemLoader):
     default_item_class = dict
     url_out = TakeFirst()
-    title_in = Join("")
+    title_in = Compose(get_title)
     title_out = TakeFirst()
     website_out = TakeFirst()
     activity_out = MapCompose(get_activity)
